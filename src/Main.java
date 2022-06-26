3b1d9a3f96754473d85fa2f8e7caf990d81d919c
mishaf1203 <sduhHDFGH345>
2022-06-26 18:11:05 +0300

    2 коммит

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,14 @@ public class Main {
         String lastName = "Ivanov ";
         String fullName = lastName + firstName + middleName;
         System.out.println("ФИО сотрудника — " + fullName);
+
         String fullNameCapital = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameCapital);
+
+        fullName = "Иванов Семён Семёнович";
+        String fixedFullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullName);
+
     }
-}
+
+    }
