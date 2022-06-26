9a1c36b647d77968509aca58ee748a3b8e287265
mishaf1203 <sduhHDFGH345>
2022-06-26 17:29:42 +0300

    1 коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        String fullNameCapital = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameCapital);
+    }
+}
